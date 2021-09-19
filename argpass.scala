f5d55e69ee5b61cca510eb959c3c2b71a460589e
Lexi <48565664+lexilamonica@users.noreply.github.com>
2021-09-19 14:57:47 -0500

    Update argpass.scala

--- argpass.scala
+++ argpass.scala
@@ -13,23 +13,7 @@ object argpass {
       n = m
     }
   }
-
-  /* EXERCISE 1: complete the following higher-order function.
-   It has one parameter f: a function that takes an instance of RefInt
-    (see above for the definition of the class RefInt) and returns Unit (i.e., nothing interesting).
-
-  Your code must create ONLY ONE instance of RefInt, then call f three times.
-  f will update the integer stored in the instances of RefInt it is given.
-  Your code must return a tuple of the three integers provided by f in the order that they came back from calls,
-    i.e., the integer from the first call to f is the first integer in the returned tuple.
-
-   */
   def refint1(f: RefInt => Unit): (Int, Int, Int) = {
-    // TODO: Provide definition here.
-    // Example call:
-    // val r = new RefInt (0)
-    // f (r)
-    // val n : Int = r.get
     val r = new RefInt(0)
     f(r)
     val x: Int = r.get
@@ -40,19 +24,6 @@ object argpass {
 
     (x, y, z)
   }
-
-  /* EXERCISE 2: complete the following higher-order function.
-    It has one parameter f: a function that takes an instance of RefInt
-      (see above for the definition of the class RefInt) and returns Unit (i.e., nothing interesting).
-    Your code must create EXACTLY THREE instances of RefInt, then call f three times.
-    f will update the integer stored in the instances of RefInt it is given.
-
-    However, f will not do this update immediately.
-    It will only do it after the third call to f: at that point it updates all three instances of
-      RefInt that it has received as arguments so far.
-    Your code must return a tuple of the three integers provided by f in the order that they came back
-      from calls, i.e., the integer from the first call to f is the first integer in the returned tuple.
- */
   def refint2(f: RefInt => Unit): (Int, Int, Int) = {
     
     val first = new RefInt(0)
@@ -68,35 +39,19 @@ object argpass {
 
     (newFirst, newSecond, newThird)
   }
-
-  // EXERCISE 3: complete the following function.
-  // It has one parameter r: (a reference to) an instance of RefInt (see above for the definition of the class RefInt)
-  // and returns (a reference to) an instance of RefInt.
-  // Your code must increment (add 1 to) the RefInt it receives and
-  // return double the original value (stored in a separate RefInt instance) as the result.
+  
   def refint3(r: RefInt): RefInt = {
     val sum: RefInt = new RefInt(r.get * 2)
     r.set(r.get + 1)
     return sum
   }
-
-  // EXERCISE 4: complete the following function.
-  // It has two parameters
-  // - r: (a reference to) an instance of RefInt (see above for the definition of the class RefInt); AND
-  // - f: a function that accepts (a reference to) an instance of RefInt.
-  // Your code must call f with a *copy* of the RefInt r, i.e., it must make a new instance of RefInt with the same Int.
-  // Your code should return true if f has NOT changed the Int stored in the copy of r.  Otherwise it should return false.
+  
   def refint4(r: RefInt, f: RefInt => Unit): Boolean = {
     val first = new RefInt(r.get)
     f(first)
     r.get == first.get
   }
-
-  // EXERCISE 5: complete the following function.
-  // It has one parameter: a list of (references to) RefInt instances.
-  // Your code must CHANGE the argument it receives in a way that is visible to the caller.
-  // Note that this is the Scala List type, which is immutable!
-  // You can assume that the list you receive is not empty.
+  
   def refint5(xs: List[RefInt]): Unit = {
     xs match {
       case Nil => Nil
